c32bd2da7827cd143808d8a7b0faa0b433337b93
Varun <varun.parihar524@gmail.com>
2024-03-22 14:26:57 +0530

    api address changed in login/signup components

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -10,7 +10,7 @@ const Signup = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-            const response = await fetch(`http://localhost:8000/api/auth/signup`, {
+            const response = await fetch(`https://notes-backend-uzu9.onrender.com/api/auth/signup`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
